aa20c34aa2ed73464d71c5956694221dcc9d5010
Ben Benedict <Ben.Benedict@gmail.com>
2021-03-09 19:46:29 -0600

    deleted about

--- src/App.vue
+++ src/App.vue
@@ -58,8 +58,6 @@
     </div>
     <div id="nav">
       <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
     </div>
     <div class="container">
       <router-view />
